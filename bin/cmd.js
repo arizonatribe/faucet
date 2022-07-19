0d9af6970a368c2584f4baf4ab979aedfff84b82
David Nunez <arizonatribe@gmail.com>
2022-07-19 17:52:03 -0600

    Refactored to use newer dependencies

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,72 +1,73 @@
 #!/usr/bin/env node
-var faucet = require('../');
-var minimist = require('minimist');
-var defined = require('defined');
-var tapeCmd = require.resolve('tape/bin/tape');
+const path = require('path');
+const { spawn } = require('child_process');
+const parseArgs = require("@vanillas/args");
 
-var spawn = require('child_process').spawn;
-var fs = require('fs');
-var path = require('path');
+const faucet = require('../lib');
+const { parseFilePaths, isExistingFile } = require('../lib/utils');
 
-var argv = minimist(process.argv.slice(2));
-var tap = faucet({
-    width: defined(argv.w, argv.width, process.stdout.isTTY
-        ? process.stdout.columns - 5
-        : 0
-    )
-});
-process.on('exit', function (code) {
-    if (code === 0 && tap.exitCode !== 0) {
-        process.exit(tap.exitCode);
+function tapFaucet() {
+    const tapeMain = require.resolve("tape")
+    let tapeCmd = path.resolve(path.dirname(tapeMain), "bin/tape")
+
+    if (!isExistingFile(tapeCmd)) {
+        console.error("Cannot find tape installed. Please install it and try again")
+        return process.exit(1)
     }
-});
-process.stdout.on('error', function () {});
 
-if (!process.stdin.isTTY || argv._[0] === '-') {
-    process.stdin.pipe(tap).pipe(process.stdout);
-    return;
-}
+    const options = parseArgs(process.argv.slice(2));
 
-var files = argv._.reduce(function (acc, file) {
-    if (fs.statSync(file).isDirectory()) {
-        return acc.concat(fs.readdirSync(file).map(function (x) {
-            return path.join(file, x);
-        }).filter(jsFile));
+    const files = parseFilePaths(options._ || [])
+
+    if (files.length === 0) {
+        console.error('usage: `faucet [FILES]` or `| faucet`\n');
+        console.error([
+            "No test files or stdin provided",
+            "and no files in test/ or tests/ directories found."
+        ].join(" "));
+
+        return process.exit(1);
     }
-    else return acc.concat(file);
-}, []);
 
-if (files.length === 0 && fs.existsSync('test')) {
-    files.push.apply(files, fs.readdirSync('test').map(function (x) {
-        return path.join('test', x);
-    }).filter(jsFile));
-}
-if (files.length === 0 && fs.existsSync('tests')) {
-    files.push.apply(files, fs.readdirSync('tests').map(function (x) {
-        return path.join('tests', x);
-    }).filter(jsFile));
-}
+    const tap = faucet({
+      /* Grab the first non-null value (default to 0) */
+      width: [
+        options.w,
+        options.width,
+        process.stdout.isTTY
+          ? process.stdout.columns - 5
+          : 0
+      ].find(arg => arg != null)
+    });
 
-if (files.length === 0) {
-    console.error('usage: `faucet [FILES]` or `| faucet`\n');
-    console.error(
-        'No test files or stdin provided and no files in test/ or tests/'
-        + ' directories found.'
-    );
-    return process.exit(1);
-}
+    process.on('exit', (code) => {
+        if (code === 0 && tap.exitCode !== 0) {
+            process.exit(tap.exitCode);
+        }
+    });
+    process.stdout.on('error', () => {});
 
-var tape = spawn(tapeCmd, files);
-tape.stderr.pipe(process.stderr);
-tape.stdout.pipe(tap).pipe(process.stdout);
-
-var tapeCode;
-tape.on('exit', function (code) { tapeCode = code });
-process.on('exit', function (code) {
-    if (code === 0 && tapeCode !== 0) {
-        console.error('# non-zero exit from the `tape` command');
-        process.exit(tapeCode);
+    if (!process.stdin.isTTY || options._[0] === '-') {
+        process.stdin.pipe(tap).pipe(process.stdout);
+        return;
     }
-});
 
-function jsFile (x) { return /\.js$/i.test(x) }
+    const tape = spawn(tapeCmd, files);
+    tape.stderr.pipe(process.stderr);
+    tape.stdout.pipe(tap).pipe(process.stdout);
+
+    let tapeCode;
+
+    tape.on('exit', (code) => {
+        tapeCode = code;
+    });
+
+    process.on('exit', (code) => {
+        if (code === 0 && tapeCode !== 0) {
+            console.error('# non-zero exit from the `tape` command');
+            process.exit(tapeCode);
+        }
+    });
+}
+
+tapFaucet();
